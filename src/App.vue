372093747c139c217dbf7dc3c8ba2a3ae3a16427
run <run27017@126.com>
2020-06-08 15:16:39 +0800

    设置每秒自动刷新 spec.

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import SwaggerUI from 'swagger-ui'
 import 'swagger-ui/dist/swagger-ui.css'
 import HelloWorld from './components/HelloWorld.vue'
@@ -14,11 +15,27 @@ export default {
   components: {
     HelloWorld
   },
+  data () {
+    return {
+      url: 'http://localhost:3000/files/swagger.json',
+      spec: ''
+    }
+  },
   mounted () {
-    SwaggerUI({
-      domNode: this.$refs.swagger,
-      url: 'http://localhost:3000/files/swagger.json'
+    this.swagger = SwaggerUI({
+      domNode: this.$refs.swagger
     })
+    setInterval(this.reload, 1000)
+  },
+  methods: {
+    async reload () {
+      const response = await axios.get(this.url)
+      const spec = JSON.stringify(response.data)
+      if (spec !== this.spec) {
+        this.spec = spec
+        this.swagger.specActions.updateSpec(spec)
+      }
+    }
   }
 }
 </script>
